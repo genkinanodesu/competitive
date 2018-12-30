383909844f232a7c7411fd0a39c970b268370ae3
清水元喜 <genki@genkinoMacBook-Pro.local>
2018-12-30 22:26:26 +0900

    20181229

---
+++ AtCoder/caddi2018/caddi2018a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+   typedef long long ll;
+   typedef pair<ll, ll> P;
+
+   const double EPS = (1e-7);
+   const ll INF =(1e13);
+   const double PI = (acos(-1));
+   const ll MOD = ll(1e9) + 7;
+
+   #define REP(i, n) for(ll i = 0; i < (ll)(n); i++)
+   #define REPR(i, n) for(ll i = n; i > -1; i--)
+   #define FOR(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+   #define ALL(x) (x).begin(),(x).end()
+   #define SORT(x) sort((x).begin(), (x).end())
+   #define REVERSE(x) reverse((x).begin(), (x).end())
+   #define SZ(x) ((ll)(x).size())
+   #define pb push_back
+   #define mp make_pair
+
+   //chmax(a, b): a>bならaをbで更新　更新したときにtrueを返す
+   //chmin(a, b): a<bならaをbで更新　更新したときにtrueを返す
+   template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+   template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+   #define dump(x) cerr<< #x << "= " << (x) << endl;
+
+   int gcd(int a,int b){return b?gcd(b,a%b):a;};
+
+   int dx[4]={1,0,-1,0};
+   int dy[4]={0,1,0,-1};
+
+ll N;
+int main(){
+  cin >> N;
+  ll ans = 0;
+  while (N > 0){
+    ans += (N % 10 == 2);
+    N /= 10;
+  }
+  cout << ans << endl;
+
+}
