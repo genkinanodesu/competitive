dc24a035e98435ec4c8535ad1fc3910bc7ae26cf
genkinoMacBook-Pro <s.genki0605@gmail.com>
2019-05-21 18:57:06 +0900

    abc126

---
+++ AtCoder/ABC/abc126/abc126d.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+   typedef long long ll;
+   typedef pair<ll, ll> Pii;
+   typedef vector<ll> Vi;
+   typedef vector<Vi> VVi;
+
+   const double EPS = (1e-7);
+   const ll INF =(1e13);
+   const double PI = (acos(-1));
+   const ll MOD = ll(1e9) + 7;
+
+   #define REP(i, n) for(ll i = 0; i < (ll)(n); i++)
+   #define REPR(i, n) for(ll i = n; i > -1; i--)
+   #define FOR(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+   #define ALL(x) (x).begin(),(x).end()
+   #define SORT(x) sort((x).begin(), (x).end())
+   #define RSORT(x) sort((x).rbegin(), (x).rend())
+   #define REVERSE(x) reverse((x).begin(), (x).end())
+   #define SZ(x) ((ll)(x).size())
+   #define pb push_back
+   #define mp make_pair
+
+   //chmax(a, b): a>bならaをbで更新　更新したときにtrueを返す
+   //chmin(a, b): a<bならaをbで更新　更新したときにtrueを返す
+   template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+   template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+   #define dump(x) cerr<< #x << "= " << (x) << endl;
+
+   ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+   ll pow(ll a, ll b){if (b == 0) return 1; else if (b % 2 == 0) return pow(a * a, b / 2); else return pow(a * a, b / 2) * a;}
+   ll pow(ll a, ll b, ll m){if (b == 0) return 1; else if (a == 0) return 0; else if (b % 2 == 0) return (pow((a * a) % m, b / 2, m) % m); else return (pow((a * a) % m, b / 2, m) * a) % m;}
+   ll residue(ll a, ll m){return ((a % m) + m) % m;};
+
+   ll dx[4]={1,0,-1,0};
+   ll dy[4]={0,1,0,-1};
+
+const ll MAX_N = 1e5;
+vector<Pii> G[MAX_N];
+bool color[MAX_N], check[MAX_N] = {};
+
+void dfs(ll x, bool b){
+  //xを色bで塗る
+  color[x] = b; check[x] = true;
+  for(auto &e : G[x]){
+    ll y = e.first, w = e.second;
+    if(check[y]) continue;
+    (w % 2 == 0) ? dfs(y, b) : dfs(y, !b);
+  }
+}
+
+int main(){
+  ll n; cin >> n;
+  REP(i, n - 1){
+    ll u, v, w; cin >> u >> v >> w; u--; v--;
+    G[u].pb(mp(v, w)); G[v].pb(mp(u, w));
+  }
+  dfs(0, 0);
+  REP(i, n) cout << color[i] << endl;
+
+}
