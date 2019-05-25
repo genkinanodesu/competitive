f02497b3e50075814e2376d8efb4cb256ca41ae4
genki_ubuntu_on_WSL <s.genki0605@gmail.com>
2019-05-25 23:32:32 +0900

    agc127

---
+++ AtCoder/ABC/abc127/abc127e.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+   typedef long long ll;
+   typedef pair<ll, ll> Pii;
+   typedef vector<ll> Vi;
+   typedef vector<Vi> VVi;
+
+   const double EPS = (1e-7);
+   const ll INF =(1e13);
+   const double PI = (acos(-1));
+   const ll MOD = ll(1e9) + 7;
+
+   #define REP(i, n) for(ll i = 0; i < (ll)(n); i++)
+   #define REPR(i, n) for(ll i = n; i > -1; i--)
+   #define FOR(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+   #define ALL(x) (x).begin(),(x).end()
+   #define SORT(x) sort((x).begin(), (x).end())
+   #define RSORT(x) sort((x).rbegin(), (x).rend())
+   #define REVERSE(x) reverse((x).begin(), (x).end())
+   #define SZ(x) ((ll)(x).size())
+   #define pb push_back
+   #define mp make_pair
+
+   //chmax(a, b): a>bならaをbで更新　更新したときにtrueを返す
+   //chmin(a, b): a<bならaをbで更新　更新したときにtrueを返す
+   template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+   template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+   #define dump(x) cerr<< #x << "= " << (x) << endl;
+
+   ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+   ll pow(ll a, ll b){if (b == 0) return 1; else if (b % 2 == 0) return pow(a * a, b / 2); else return pow(a * a, b / 2) * a;}
+   ll pow(ll a, ll b, ll m){if (b == 0) return 1; else if (a == 0) return 0; else if (b % 2 == 0) return (pow((a * a) % m, b / 2, m) % m); else return (pow((a * a) % m, b / 2, m) * a) % m;}
+   ll residue(ll a, ll m){return ((a % m) + m) % m;};
+
+   ll dx[4]={1,0,-1,0};
+   ll dy[4]={0,1,0,-1};
+
+ll comb(ll a, ll b){
+  ll ret = 1;
+  REP(i, b){
+    ret *= (a - b + i + 1); ret %= MOD;
+    ret *= pow(i + 1, MOD - 2, MOD); ret %= MOD;
+  }
+  return ret;
+}
+int main(){
+  ll n, m, k; cin >> n >> m >> k;
+  ll ans = comb(n + 1, 3) * (m * m % MOD) + comb(m + 1, 3) * (n * n % MOD); ans %= MOD;
+  ans *= comb(n * m - 2, k - 2); ans %= MOD;
+  cout << ans << endl;
+}
